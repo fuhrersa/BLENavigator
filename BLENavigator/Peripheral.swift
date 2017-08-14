1fc2ebacafc80554cbc6c0cd93770d98920ac410
Samuel Fuhrer <sfu@samuels-macbook-pro.local>
2017-08-14 15:35:53 +0200

    initial version

---
+++ BLENavigator/Peripheral.swift
@@ -0,0 +1,18 @@
+//
+//  Peripheral.swift
+//  BLENavigator
+//
+//  Created by Samuel Fuhrer on 8/14/17.
+//  Copyright © 2017 Samuel Fuhrer. All rights reserved.
+//
+
+
+class Peripheral {
+    //MARK: Properties
+    var name: String
+    
+    //MARK: Initializayion
+    init (name: String) {
+        self.name = name
+    }
+}
